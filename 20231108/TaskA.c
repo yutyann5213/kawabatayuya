8ed88449c2b0edd634d4b8874461104b62701f51
yutyann5213 <yutyanntyenn5213@gmail.com>
2023-11-15 15:32:05 +0900

    ³ード以外の削除

---
+++ 20231108/TaskA.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int i;
+
+    for(i=1;i<=10;i++)
+    {
+       printf("%d\n");
+    }
+
+    return 0;
+}
---
+++ 20231108/TaskC.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main ( )  {
+  int deta [ 10] ;
+  for  ( int i =  0 ; i <  10 ; i ++ )  {
+    deta [ i ]  =  rand ( )  %  9  +  1 ;
+  }
+  for  ( int i =  9 ; i >=  0 ; i -- )  {
+    printf ( "%d \n " , deta [ i ] ) ;
+  }
+   return 0;
+}
